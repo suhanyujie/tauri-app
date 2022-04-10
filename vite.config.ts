e65107f7cdbedad614ea5e47c64d419439ccf2b2
suhanyujie <suhanyu@bjx.cloud>
2022-04-10 10:26:46 +0800

    update some code

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    const root = process.cwd()
+
+    return {
+      // dev 独有配置
+      root,
+      base: VI
+    }
+  } else {
+    // command === 'build'
+    return {
+      // build 独有配置
+    }
+  }
 })
